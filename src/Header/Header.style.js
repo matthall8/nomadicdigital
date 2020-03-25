6939eebfe3ba2332a5a04be0369873880c1cfeb0
Matt Hall <matthall8@gmail.com>
2020-03-25 09:11:36 +0000

    Added structure of components for the homepage

---
+++ src/Header/Header.style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+  height: 250px;
+  background-image: url(https://user-images.githubusercontent.com/28222381/45626954-72961080-ba99-11e8-9215-538504f985ea.jpg);
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 250px;
+  background-position: bottom;
+  @media only screen and (min-width: 600px) {
+    background-image: url(https://user-images.githubusercontent.com/28222381/45878093-906cb980-bda8-11e8-9534-61322b3c14c7.png);
+}
+`;
