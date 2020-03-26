3ae0c2972e1492ca3c1b7cca206905c35133d199
Matt Hall <matthall8@gmail.com>
2020-03-26 08:14:04 +0000

    Added body styling and header styling

---
+++ src/Body/Body.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { createGlobalStyle } from 'styled-components'
+
+const StyledBody = createGlobalStyle`
+  body {
+    padding: 0;
+    margin: 0;
+  }
+`
+
+export default StyledBody;
