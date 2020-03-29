90d14fa8870f9fa1067f0930b7a6ba587fb75933
Matt Hall <matthall8@gmail.com>
2020-03-29 15:24:19 +0100

    Added placeholder contact section

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,11 +1,40 @@
 import React from "react";
-// import {StyledHeader, Title, Tagline, Logo} from "./HomeHeader.style";
+import {ContactSection, ContactH2,LoginInput, Textarea, Submit} from "./Contact.style";
 // import logo from "../../../../assets/logoDarkBlue.svg";
 
-const Contact = () => {
-  return (
-    <h2>Contact</h2>
-  );
+class Contact extends React.Component {
+  render() {
+    return(<ContactSection>
+      <ContactH2>Contact</ContactH2>
+      <label htmlFor="name">Name</label>
+            <LoginInput
+              component="input"
+              placeholder="Your Name"
+              type="text"
+              name="name"
+              id="name"
+            />
+                 <label htmlFor="email">Email</label>
+            <LoginInput
+              component="input"
+              placeholder="Your Email"
+              type="email"
+              name="email"
+              id="email"
+            />
+                             <label htmlFor="number">Phone number</label>
+            <LoginInput
+              component="input"
+              placeholder="Your Number"
+              type="phonenumber"
+              name="number"
+              id="number"
+            />
+            <Textarea placeholder="Tell us a little bit about your project">
+            </Textarea>
+            <Submit>Say Hello!</Submit>
+    </ContactSection>)
+  };
 };
 
-export default Contact;
+export default Contact
--- src/Experience/Experience.style.js
+++ src/Experience/Experience.style.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const ExperienceSection = styled.section` 
   text-align: center;
+  margin: 0px 0px 20px 0px;
   img {
     margin: 50px auto 0px auto;
     display: block;
