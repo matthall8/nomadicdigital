21ad4cb73f9e5f29dc4c3d5e0d7dcef2baeeed45
Matt Hall <matthall8@gmail.com>
2020-03-30 08:58:28 +0100

    Added dev ops configuration to work with Netlify

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,33 @@
 const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
 module.exports = {
     // define entry file and output
     entry: './src/index.js',
     output: {
-        path: path.resolve('dist'),
-        filename: 'main.js'
+        // move bundle.js to a folder instead the root
+        path: path.resolve('./build'),
+        filename: 'bundle.js'
     },
     // define babel loader
     module: {
         rules: [
             { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ }
         ]
-    }
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+      },
+    plugins: [
+        // New plugin
+        new HtmlWebpackPlugin({
+          // injects bundle.js to our new index.html
+          inject: true,
+          // copys the content of the existing index.html to the new /build index.html
+          template:  path.resolve('./index.html'),
+        }),
+      ]
  };
