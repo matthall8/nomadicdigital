51d24e789a5c7ea4fad02a9a98eb471576f4a453
Matt Hall <matthall8@gmail.com>
2020-04-02 09:09:42 +0100

    Updated structure and web font

---
+++ src/Components/H2/H2.style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledH2 = styled.h2`
+  border-bottom: #D19C1D 4px solid;
+  font-size: 2.1rem;
+  padding: 20px 0px;
+  width: fit-content;
+  font-family: 'Muli', sans-serif;
+  margin: 0 auto;
+  width: 45%;
+`;
