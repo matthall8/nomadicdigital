5c6314141938a3d02c8043de1948d160ce3904f9
Matt Hall <matthall8@gmail.com>
2020-11-12 18:31:47 +0000

    Image optimisation set up using webpack

--- webpack.config.js
+++ webpack.config.js
@@ -19,9 +19,30 @@ module.exports = {
            {
             test: /\.(png|jpe?g|gif)$/i,
             use: [
-              {
-                loader: 'file-loader',
-              },
+                'file-loader', 
+                {
+                  loader: 'image-webpack-loader',
+                  options: {
+                    mozjpeg: {
+                      progressive: true,
+                    },
+                    // optipng.enabled: false will disable optipng
+                    optipng: {
+                      enabled: false,
+                    },
+                    pngquant: {
+                      quality: [0.65, 0.90],
+                      speed: 4
+                    },
+                    gifsicle: {
+                      interlaced: false,
+                    },
+                    // the webp option will enable WEBP
+                    webp: {
+                      quality: 75
+                    }
+                  }
+                },
             ],
           },
           {
