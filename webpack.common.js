7ff18dbedc0a80e5c4947204d24635bf9ddd72a5
Matt Hall <matthall8@gmail.com>
2020-11-13 09:50:10 +0000

    webpack

--- webpack.common.js
+++ webpack.common.js
@@ -12,6 +12,55 @@ module.exports = {
     app: './src/index.js',
 
   },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+module: {
+  rules: [
+      { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/,
+     }, 
+     {
+      test: /\.(png|jpe?g|gif)$/i,
+      use: [
+          'file-loader', 
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75
+              }
+            }
+          },
+      ],
+    },
+    {
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+    },
+  ]
+},
+
+optimization: {
+  minimize: true,
+  minimizer: [new TerserPlugin()],
+},
 
   plugins: [
 
@@ -26,13 +75,5 @@ module.exports = {
     }),
 
   ],
-
-  output: {
-
-    filename: '[name].bundle.js',
-
-    path: path.resolve(__dirname, 'dist'),
-
-  },
 
 };
