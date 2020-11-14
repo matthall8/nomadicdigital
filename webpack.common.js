7369955d93e7bd8c9b38ded2efdcfefbefe88cc5
Matt Hall <matthall8@gmail.com>
2020-11-14 16:03:17 +0000

    Upgraded to webpack v5

--- webpack.common.js
+++ webpack.common.js
@@ -1,77 +1,61 @@
-const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
-
-  entry: {
-
-    app: './src/index.js',
-
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-
-module: {
-  rules: [
-      { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/,
-     }, 
-     {
-      test: /\.(png|jpe?g|gif)$/i,
-      use: [
-          'file-loader', 
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              mozjpeg: {
-                progressive: true,
-              },
-              // optipng.enabled: false will disable optipng
-              optipng: {
-                enabled: false,
-              },
-              pngquant: {
-                quality: [0.65, 0.90],
-                speed: 4
-              },
-              gifsicle: {
-                interlaced: false,
-              },
-              // the webp option will enable WEBP
-              webp: {
-                quality: 75
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+            'file-loader', 
+            {
+              loader: 'image-webpack-loader',
+              options: {
+                mozjpeg: {
+                  progressive: true,
+                },
+                // optipng.enabled: false will disable optipng
+                optipng: {
+                  enabled: false,
+                },
+                pngquant: {
+                  quality: [0.65, 0.90],
+                  speed: 4
+                },
+                gifsicle: {
+                  interlaced: false,
+                },
+                // the webp option will enable WEBP
+                webp: {
+                  quality: 75
+                }
               }
-            }
-          },
-      ],
-    },
-    {
-      test: /\.css$/i,
-      use: ['style-loader', 'css-loader'],
-    },
-  ]
-},
-
-optimization: {
-  minimize: true,
-  minimizer: [new TerserPlugin()],
-},
-
+            },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
   plugins: [
-
+    new BundleAnalyzerPlugin(),
     new CleanWebpackPlugin(),
-
     new HtmlWebpackPlugin({
-
       title: 'Production',
       template: './src/index.html'
-
     }),
-
   ],
-
-};
+  };
