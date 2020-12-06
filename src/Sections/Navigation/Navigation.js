80e53a553ce9d4d95d76229f3f0800a029fd96ce
Matt Hall <matthall8@gmail.com>
2020-12-06 17:11:36 +0000

    RightNav moved to navigation and now closes on link click

--- src/Sections/Navigation/Navigation.js
+++ src/Sections/Navigation/Navigation.js
@@ -1,20 +1,25 @@
 import React, {useState, useRef} from "react";
-import {BurgerMenu} from "./Navigation.style";
-import RightNav from '../RightNav/RightNav.js';
+import {BurgerMenu, Ul, ContactLi} from "./Navigation.style";
 import { useOnClickOutside } from '../../hooks';
 
 const Navigation = () => {
-const [open, setOpen] = useState(false)
-const node = useRef(); 
-useOnClickOutside(node, () => setOpen(false));
-  return (
+  const [open, setOpen] = useState(false)
+  const node = useRef(); 
+  useOnClickOutside(node, () => setOpen(false));
+    return (
     <nav ref={node}>
       <BurgerMenu open={open} onClick={() => setOpen(!open)}>
         <div />
         <div />
         <div />
       </BurgerMenu>
-      <RightNav open={open}/>
+      <Ul open={open}>
+        <li><a href="#" onClick={() => setOpen(!open)}>Home</a></li>
+        <li><a href="#services" onClick={() => setOpen(!open)}>Services</a></li>
+        <li><a href="#experience" onClick={() => setOpen(!open)}>Experience</a></li>
+        <li><a href="#locations" onClick={() => setOpen(!open)}>Work Locations</a></li>
+        <ContactLi><a href="#contact" onClick={() => setOpen(!open)}>Contact Us</a></ContactLi>
+      </Ul>
     </nav>
   );
 };
--- src/Sections/Navigation/Navigation.style.js
+++ src/Sections/Navigation/Navigation.style.js
@@ -18,6 +18,7 @@ export const BurgerMenu = styled.div`
     height: 0.25rem;
     background-color: ${({ open }) => open ? '#000' : '#fff'};
     border-radius: 10px;
+    border: 0.75px solid #1E282A;
     transform-origin: 1px;
     transition: all 0.3s linear;
     &:nth-child(1) {
@@ -32,3 +33,44 @@ export const BurgerMenu = styled.div`
     }
 }
 `;
+
+export const Ul = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-flow: row nowrap;
+  a {
+    color:#fff;
+  }
+  li {
+    padding: 18px 10px;
+  }
+  @media (max-width: 768px) {
+    font-family: 'Muli', sans-serif;
+    margin: 0;
+    flex-flow: column nowrap;
+    background-color: #fff;
+    position: fixed;
+    transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(100%)'};
+    top: 0;
+    right: 0;
+    width: 100%;
+    padding: 0rem 0 0 0;
+    color: #1E282A;
+    a {
+      color: #1E282A;
+    }
+    transition: transform 0.3s ease-in-out;
+    li {
+      font-weight: bold;
+      border-bottom: #D19C1D 1px solid;
+    }
+  }
+`;  
+
+export const ContactLi = styled.li`
+  @media only screen and (min-width: 600px) {
+      padding: 14px 0px 14px 10px;
+      border: 2px solid #fff;
+      border-radius: 10px;
+  }
+`
